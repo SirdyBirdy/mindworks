8e3d1ef640bb038e87a4724f23b85fff395ebf9e
SirdyBirdy <scotiawebdesigns@gmail.com>
2026-05-28 13:43:55 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://mindworkscounselling.com',
+  site: 'https://sirdybirdy.github.io',
+  base: '/mindworks',
 });
